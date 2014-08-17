12ff016bd620b52805c83d72dde64cc3c2b4ccb2
Andy Li <andy@onthewings.net>
2014-08-17 17:51:43 +0800

    0.0.7: updated to work with github changes

--- src/ContentScript.hx
+++ src/ContentScript.hx
@@ -18,54 +18,63 @@ class ContentScript {
 	static function processGithubBlobDiv(blobDiv:DivElement):Void {
 		var url = cast (new JQuery(blobDiv).parent(".file").find("#raw-url, .raw-url")[0], AnchorElement).href;
 		// trace(url);
-		var container = new JQuery(blobDiv).find(".highlight pre").parent();
-		var firstlineid = container.find(".line:first").attr("id");
+		var container = new JQuery(blobDiv).find(".highlight");
+		var firstlineid = container.find(".js-line-number:first, .line:first").attr("id");
 		var lineidprefix = firstlineid.substring(0, firstlineid.lastIndexOf("1"));
 		container
-			.data("pygmentx-original", container.html())
-			.load('http://pygmentx.onthewings.net/ .highlight pre',
-				JQuery._static.param({
-					url: url,
-					lineidprefix: lineidprefix
-				}),
-				function(responseText, textStatus, XMLHttpRequest){
-					if (textStatus != "success") {
-						trace(textStatus);
-						trace(responseText);
-						return;
-					}
-					container
-						.data("pygmentx-new", container.html())
-						.data("pygmentx-current", "pygmentx-new");
-					
-					var buttonGroup = new JQuery(blobDiv).siblings(".meta").find(".button-group");
-					var toggleBtn = new JQuery("<a href='#' class='minibutton tooltipped tooltipped-s' aria-label='Pygmentx enabled'><span class='label'>Pygmentx</span></a>")
-						.click(function(evt:Event){
-							if (container.data("pygmentx-current") == "pygmentx-new") {
-								container.html(container.data("pygmentx-original"));
-								container.data("pygmentx-current", "pygmentx-original");
-								new JQuery(evt.currentTarget)
-									.attr("aria-label", "Pygmentx disabled")
-								.find(".label")
-									.fadeTo(0, 0.5);
-							} else {
-								container.html(container.data("pygmentx-new"));
-								container.data("pygmentx-current", "pygmentx-new");
-								new JQuery(evt.currentTarget)
-									.attr("aria-label", "Pygmentx enabled")
-								.find(".label")
-									.fadeTo(0, 1);
-							}
-							evt.preventDefault();
-						})
-						.appendTo(buttonGroup);
-					if (buttonGroup.find("a.button").length > 0) {
-						toggleBtn.addClass("button");
-					}
-				}
-			);
-	}
+			.data("pygmentx-original", container.html());
 
+		JQuery._static.ajax('http://pygmentx.onthewings.net/', {
+			data: {
+				url: url,
+				lineidprefix: lineidprefix
+			},
+			success: function(responseText:String, textStatus, XMLHttpRequest){
+				if (textStatus != "success") {
+					trace(textStatus);
+					trace(responseText);
+					return;
+				}
+
+				var newContent = new JQuery(responseText);
+
+				container.find(".blob-line-code").each(function(i:Int, e:Node) {
+					var je = new JQuery(e);
+					var lineno = Std.parseInt(je.attr("id").substring("LC".length));
+					je.html(newContent.find('#${lineidprefix}${lineno}').html());
+				});
+				
+				var buttonGroup = new JQuery(blobDiv).siblings(".meta").find(".button-group");
+				var toggleBtn = new JQuery("<a href='#' class='minibutton tooltipped tooltipped-s' aria-label='Pygmentx enabled'><span class='label'>Pygmentx</span></a>")
+					.click(function(evt:Event){
+						if (container.data("pygmentx-current") == "pygmentx-new") {
+							container.html(container.data("pygmentx-original"));
+							container.data("pygmentx-current", "pygmentx-original");
+							new JQuery(evt.currentTarget)
+								.attr("aria-label", "Pygmentx disabled")
+							.find(".label")
+								.fadeTo(0, 0.5);
+						} else {
+							container.html(container.data("pygmentx-new"));
+							container.data("pygmentx-current", "pygmentx-new");
+							new JQuery(evt.currentTarget)
+								.attr("aria-label", "Pygmentx enabled")
+							.find(".label")
+								.fadeTo(0, 1);
+						}
+						evt.preventDefault();
+					})
+					.appendTo(buttonGroup);
+				if (buttonGroup.find("a.button").length > 0) {
+					toggleBtn.addClass("button");
+				}
+
+				container
+					.data("pygmentx-new", container.html())
+					.data("pygmentx-current", "pygmentx-new");
+			}
+		});
+	}
 	
 	static function main():Void {
 		switch (Browser.location.hostname) {
